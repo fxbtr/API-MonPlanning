c7d2e35c22a371dd29c9e9091129a8edc01edff5
Audrey <a.comlancataria@epsi.fr>
2019-04-11 10:32:45 +0200

    Class Employee created

---
+++ MonPlanning/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MonPlanning.Models
+{
+    public class Employee
+    {
+        private string firstName, lastName;
+        private int id;
+
+        public Employee(int _id, string _firstName, string _lastName)
+        {
+            id = _id;
+            firstName = _firstName;
+            lastName = _lastName;
+        }
+    }
+}
