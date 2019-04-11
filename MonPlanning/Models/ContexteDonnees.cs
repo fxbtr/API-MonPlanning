de982fd7060cd0c50f00dced48f4cb97b8d61184
Audrey <a.comlancataria@epsi.fr>
2019-04-11 18:04:23 +0200

    Data model created

---
+++ MonPlanning/Models/ContexteDonnees.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MonPlanning.Models
+{
+    public class ContexteDonnees
+    {
+        private Dictionary<int, Employee> employeesList;
+
+        public ContexteDonnees()
+        {
+            employeesList = new Dictionary<int, Employee>();
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            employeesList.Add(employee.Id ,employee);
+        }
+
+        public IReadOnlyDictionary<int, Employee> EmployeeById => employeesList;
+    }
+}
