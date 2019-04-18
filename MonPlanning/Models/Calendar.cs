8e1ae4ed2cf2c52776c5dbd8fed88c20e8ccc458
Audrey <a.comlancataria@epsi.fr>
2019-04-18 17:45:26 +0200

    Days set in a table

--- MonPlanning/Models/Calendar.cs
+++ MonPlanning/Models/Calendar.cs
@@ -9,16 +9,24 @@ namespace MonPlanning.Models
     {
         private string month;
         private int daysCount, code;
+        private int[] days;
 
         public Calendar(int _code, string _month, int _daysCount)
         {
             code = _code;
             month = _month;
             daysCount = _daysCount;
+            days = new int[_daysCount];
+
+            for (var i = 0; i < _daysCount; i++)
+            {
+                days[i] = i + 1;
+            }
         }
 
         public string Month => month;
         public int Code => code;
         public int DaysCount => daysCount;
+        public int[] Days => days;
     }
 }
